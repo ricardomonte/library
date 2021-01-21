dd3e326e5b2e7d0636334b673d3797599b4c7983
ricardo <ramn21@gmail.com>
2021-01-22 00:07:20 +0100

    add change requires

--- assets/javascript/library.js
+++ assets/javascript/library.js
@@ -1,13 +1,14 @@
-const listBook = [];
+let listBook = [];
 let booki = '';
-
+let localStorageItems = '';
 const bookListUl = document.querySelector('#book-list ul');
 const form = document.forms['add-book'];
 const u = document.querySelector('#container-li');
 const btn = document.querySelector('#g-form');
 
+
 function Books(title, author, pages, read) {
-  if (read === 'allready readed') {
+  if (read === 'Allready readed') {
     read = true;
   } else {
     read = false;
@@ -25,8 +26,54 @@ Books.prototype.info = function () {
   Status: ${this.read}`;
 };
 
+function displayBook(title, author, pages, read) {
+  const l = document.createElement('li');
+  const titl = document.createElement('p');
+  const auth = document.createElement('p');
+  const pag = document.createElement('p');
+  const rea = document.createElement('button');
+  const buttondelete = document.createElement('button');
+  titl.textContent = `Title: ${title}`;
+  auth.textContent = `Author: ${author}`;
+  pag.textContent = `Nº of pages: ${pages}`;
+  rea.textContent = read;
+  buttondelete.textContent = 'Delete';
+  buttondelete.classList.add('delete');
+  rea.classList.add('changes');
+  rea.id = 'changeble';
+  l.classList.add('book');
+  l.appendChild(titl);
+  l.appendChild(auth);
+  l.appendChild(pag);
+  l.appendChild(rea);
+  l.appendChild(buttondelete);
+  u.appendChild(l);
+}
+
+localStorageItems = localStorage.getItem('book');
+if (localStorageItems) {
+  listBook = JSON.parse(localStorageItems);
+  /* eslint-disable */
+  for (let j = 0; j < listBook.length; j++) {
+    const { title } = listBook[j];
+    const { author } = listBook[j];
+    const { pages } = listBook[j];
+    const read = (listBook[j].read === true) ? 'Allready readed' : 'Not readed';
+    displayBook(title, author, pages, read);
+  }
+  /* eslint-enable */
+}
+
 function AddBookToList(something) {
-  listBook.push(something);
+  const localContainer = localStorage.getItem('book');
+  let book;
+  if (localContainer === null) {
+    book = [];
+  } else {
+    book = JSON.parse(localContainer);
+  }
+  book.push(something);
+  localStorage.setItem('book', JSON.stringify(book));
 }
 
 const changeStatus = (e) => {
@@ -59,30 +106,6 @@ function displayForm() {
   }
 }
 
-function displayBook(title, author, pages, read) {
-  const l = document.createElement('li');
-  const titl = document.createElement('p');
-  const auth = document.createElement('p');
-  const pag = document.createElement('p');
-  const rea = document.createElement('button');
-  const buttondelete = document.createElement('button');
-  titl.textContent = `Title: ${title}`;
-  auth.textContent = `Author: ${author}`;
-  pag.textContent = `Nº of pages: ${pages}`;
-  rea.textContent = read;
-  buttondelete.textContent = 'Delete';
-  buttondelete.classList.add('delete');
-  rea.classList.add('changes');
-  rea.id = 'changeble';
-  l.classList.add('book');
-  l.appendChild(titl);
-  l.appendChild(auth);
-  l.appendChild(pag);
-  l.appendChild(rea);
-  l.appendChild(buttondelete);
-  u.appendChild(l);
-}
-
 bookListUl.addEventListener('click', (e) => {
   remove(e);
   changeStatus(e);
@@ -98,14 +121,13 @@ form.addEventListener('submit', (e) => {
   let author = document.querySelector('#author').value;
   let pages = document.querySelector('#pages').value;
   let status = document.querySelector('#status').value;
+  booki = new Books(title, author, pages, status);
   displayForm();
   displayBook(title, author, pages, status);
-  booki = new Books(title, author, pages, status);
   AddBookToList(booki);
   title = '';
   author = '';
   pages = '';
   status = '';
-
   form.reset();
 });
